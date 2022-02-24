d182bd63d9f5e114e64259f16fa837817870ac76
Luiza Ruivo Marinho Santos <luiza.santos@ambevtech.com.br>
2022-02-23 21:49:16 -0300

    karma unit reporter

--- karma.conf.js
+++ karma.conf.js
@@ -10,8 +10,9 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
+      require('karma-junit-reporter'),
       require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
